227d47750e0da763398c979d3c881fa3c76434b9
unknown <greenianda@gmail.com>
2015-05-20 12:37:18 +0700

    ubah side menu doang

--- application/views/operator/side_menu.php
+++ application/views/operator/side_menu.php
@@ -112,7 +112,7 @@
                 	</ul>
                 </li>
 
-			<li class="<?php if($page_name == 'harian1' || $page_name== 'harian2'  )echo 'active'; ?>dropdown"><a href=""><span class="icon-briefcase"></span>Laporan Harian</a>
+			<li class="<?php if($page_name == 'harian1' || $page_name== 'harian2'  )echo 'active'; ?>dropdown"><a href=""><span class="icon-briefcase"></span>Laporan Tambahan</a>
                 	<ul style="<?php if($page_name == 'harian1' || $page_name== 'harian2'  )echo 'display: block'; ?>">
 						<li><a href="<?php echo base_url(); ?>cont_cetak_lap_harian/register_harian">Register Pasien Harian</a></li>
 						<li><a href="<?php echo base_url(); ?>cont_cetak_lap_mingguan/rekap_penyakit">Rekap Penyakit Mingguan</a></li>			
